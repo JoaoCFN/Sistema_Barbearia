c2d79fd9d9acfc56b28dfa7bc85eee3c595dabe7
EliabeSilva <61918977+EliabeSilva@users.noreply.github.com>
2020-11-03 18:53:40 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","dbtcc");
+    $conn = mysqli_connect("localhost", "root", "", "dbtcc1");
     $nome = mysqli_real_escape_string($conn, $_POST['usuario']);
     $telefone = mysqli_real_escape_string($conn, $_POST['telBar']);
     $email = mysqli_real_escape_string($conn, $_POST['email']);
@@ -15,20 +15,18 @@
     $ufBar = mysqli_real_escape_string($conn, $_POST['ufBar']);
     $cepBar = mysqli_real_escape_string($conn, $_POST['cepBar']);
 
-    $queryCpf = "select cpf from usuario where cpf = '{$cpf}'";
+    $queryCpf = "select cpf_dono from barbearia where cpf_dono = '{$cpf}'";
     $resultCpf = mysqli_query($conn, $queryCpf);
     $rowCpf = mysqli_num_rows($resultCpf);
     //
-    $queryEmail = "select email from usuario where email = '{$email}'";
+    $queryEmail = "select email_dono from barbearia where email_dono = '{$email}'";
     $resultEmail = mysqli_query($conn, $queryEmail);
     $rowEmail = mysqli_num_rows($resultEmail);
 
-    if ($rowCpf == 1 || $rowEmail ==1 ) {
-        echo '<script>window.alert("Houve algum erro no seu Cadastro.");</script>"';
-    // }elseif(strlen($nome)<3 || strlen($telefone)<3 || strlen($email<3) || strlen($senha)<3 || strlen($datanasc)<3 || strlen($cpf)<3 ){
-    //     echo '<script>window.alert("Houve algum erro no seu Cadastro.");</script>"';
+    if ($rowCpf == 1 || $rowEmail == 1) {
+        echo "<script>window.alert('Houve algum erro no seu Cadastro.');</script>";
     }else{
-        $insert = ("INSERT INTO usuario (nome, email, senha, nome_Bar, cpf, telefone, cnpj, rua, numero, bairro, uf, cep, cidade) VALUES ('$nome', '$email', '$senha', '$nameBar', '$cpf', '$telefone', '$cnpjBar', '$ruaBar', '$nBar', '$bairroBar', '$ufBar', '$cepBar', '$cidadeBar')");
+        $insert = ("INSERT INTO barbearia (nome_dono, email_dono, senha_dono, nome_barbearia, cpf_dono, telefone, cnpj, rua, num_bar, bairro, uf, cep, cidade) VALUES ('$nome', '$email', '$senha', '$nameBar', '$cpf', '$telefone', '$cnpjBar', '$ruaBar', '$nBar', '$bairroBar', '$ufBar', '$cepBar', '$cidadeBar')");
 
         $run_insert = mysqli_query($conn, $insert);
         require_once "conteudo/registro/sucesso.php";
