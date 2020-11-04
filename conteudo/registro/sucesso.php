d92c32d5f23ca0ce8bc139eff4aaa76da0a5bdc3
Genivaldo Alves <geniinho@Hotmail.com>
2020-11-04 10:27:27 -0300

    redirecionar p o login ao finalizar cadastro

--- conteudo/registro/sucesso.php
+++ conteudo/registro/sucesso.php
@@ -2,9 +2,12 @@
 
 <script>
 
-Swal.fire(
-  'Parabéns',
-  'Seu cadastro foi realizado com sucesso.',
-  'success'
-)
+Swal.fire({
+  icon: 'success',
+  title: 'Parabéns',
+  text: 'Seu cadastro foi realizado com sucesso!'
+}).then(function() {
+    window.location = "login.php";
+});
+
 </script>
