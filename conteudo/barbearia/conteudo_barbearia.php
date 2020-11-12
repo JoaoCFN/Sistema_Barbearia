8019815e33aafb6c7a638ccd898932d28162b9ea
João Neto <joaoneto.20fsa@gmail.com>
2020-11-12 00:01:29 -0300

    Construindo a seção de agendamento de serviço

---
+++ conteudo/barbearia/conteudo_barbearia.php
@@ -0,0 +1,10 @@
+<section class="gap-to-menu container">
+    <h1 class="sb-txt-white">Teste</h1>
+
+    <button class="btn-whatsapp btn-position-fixed">
+        <a href="https://api.whatsapp.com/send?phone=5575988383174" target="_blank">
+            <i class="fa fa-whatsapp"></i>
+        </a>
+    </button>
+</section>
+
