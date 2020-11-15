c4c71732683ea0b28b15e3d6111964cecb107dae
João Neto <joaoneto.20fsa@gmail.com>
2020-11-14 22:15:47 -0300

    Update barbearia_form.js

--- js/barbearia/barbearia_form.js
+++ js/barbearia/barbearia_form.js
@@ -1,13 +1,27 @@
-// Data
-$('.datepicker').pickadate({
-    formatSubmit: 'yyyy/mm/dd',
-    hiddenPrefix: 'prefix__',
-    hiddenSuffix: '__suffix',
-    klass: {
-        navPrev: 'picker__nav--next',
-        navNext: 'picker__nav--prev',
-    }
-})
+(function barbearia_form(){
+    const data = new Date();
+    /* 
+    O mês do JS é contado de 0-11. Então para a lógica funcionar 
+    corretamente buscando limitar o mês anterior, é preciso fazer
+    o teste lógico abaixo
+    */
+    const mesAtual = data.getMonth();
+    const mesInvalido = mesAtual == 0 ? 12: mesAtual;
+
+    const diaAtual = data.getDate();
+
+    // Data
+    $('.datepicker').pickadate({
+        formatSubmit: 'yyyy/mm/dd',
+        hiddenPrefix: 'prefix__',
+        hiddenSuffix: '__suffix',
+        klass: {
+            navPrev: 'picker__nav--next',
+            navNext: 'picker__nav--prev',
+        },
+        min: new Date(2020, mesInvalido, diaAtual)
+    })
+})();
 
 function handleButton(target){
     const btnAttribute = target.getAttribute("data-target-title");
