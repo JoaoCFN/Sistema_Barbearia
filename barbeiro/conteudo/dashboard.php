9af40fc148f7400c0cf9092b7cc71b7f68942a24
João Neto <joaoneto.20fsa@gmail.com>
2020-11-27 00:06:26 -0300

    Ajustando a responsividade

--- barbeiro/conteudo/dashboard.php
+++ barbeiro/conteudo/dashboard.php
@@ -1,41 +1,41 @@
 <?php 
 session_start();
 $barber_id = $_SESSION['barbearia_id'];
-print_r($_SESSION);
-echo "ID BARBEARIA ".$barber_id;
+// print_r($_SESSION);
+// echo "ID BARBEARIA ".$barber_id;
 $selectServicosDia = "SELECT * FROM agendamento where barbearia = $barber_id";
 $query = $mysqli -> query ($selectServicosDia);
 $row = mysqli_fetch_assoc($query); 
-print_r ($row);
+// print_r ($row);
 ?>
 <div class="home-main">
     <div class=" container-fluid">
-        <div class="row  pt-5 align-items-start">
-            <div class="col-lg-4 col-md-8 col-sm-8 mb-5 mr-3 ml-3 card" style="flex: 0 0 31.99%;">
-                <div class="row">
+        <div class="row pt-5 align-items-start">
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="bg-white">
                     <div class="col-12 headerIcon pt-3 pb-3">
-                        <span><i class="fas fa-clock"></i> Serviços agendados para o dia</span>
-                    </div>
-                    <div class="col-12 text-right mt-2">
-                        <p class="card-category">
-                            Total de serviços
-                        </p>
-                        <p class="card-data">
-                            42
-                        </p>
-                    </div>
-                    <hr class="hr-card">
-                    <div class="col-md-12 cd-footer mb-3">
-                        <p>
-                            <i class="fas fa-eye"></i>
-                            Visualizar os serviços diarios.
-                        </p>
+                            <span><i class="fas fa-clock"></i> Serviços agendados para o dia</span>
+                        </div>
+                        <div class="col-12 text-right mt-2">
+                            <p class="card-category">
+                                Total de serviços
+                            </p>
+                            <p class="card-data">
+                                42
+                            </p>
+                        </div>
+                        <hr class="hr-card">
+                        <div class="col-md-12 cd-footer mb-3">
+                            <p>
+                                <i class="fas fa-eye"></i>
+                                Visualizar os serviços diarios.
+                            </p>
                     </div>
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-8 col-sm-8 mb-5 mr-3 ml-3 card" style="flex: 0 0 31.99%;">
-                <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="bg-white">
                     <div class="col-12 headerIcon pt-3 pb-3">
                         <span><i class="fas fa-money-bill-wave"></i> Lucro diario</span>
                     </div>
@@ -57,8 +57,8 @@ print_r ($row);
                 </div>
             </div>
 
-            <div class="col-lg-4 col-md-8 col-sm-8 mb-5 mr-3 ml-3 card" style="flex: 0 0 31.99%;">
-                <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
+                <div class="bg-white">
                     <div class="col-12 headerIcon pt-3 pb-3">
                         <span><i class="fas fa-clock"></i> Serviço mais requisitado</span>
                     </div>
@@ -79,6 +79,7 @@ print_r ($row);
                     </div>
                 </div>
             </div>
+
         </div>
 
         <div class="mt-5 mb-5 pb-5 container-fluid">
