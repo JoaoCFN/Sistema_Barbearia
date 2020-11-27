9ae4d52d91d55673c07ba06f8d0685dae5e3d2f6
João Neto <joaoneto.20fsa@gmail.com>
2020-11-26 23:36:44 -0300

    banco atualizado

--- dbtcc.sql
+++ dbtcc.sql
@@ -38,7 +38,7 @@ CREATE TABLE `agendamento` (
   KEY `fk_barbearia_id` (`barbearia`),
   CONSTRAINT `fk_barbearia_id` FOREIGN KEY (`barbearia`) REFERENCES `barbearia` (`barbearia_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`usuario`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +47,7 @@ CREATE TABLE `agendamento` (
 
 LOCK TABLES `agendamento` WRITE;
 /*!40000 ALTER TABLE `agendamento` DISABLE KEYS */;
-INSERT INTO `agendamento` VALUES (1,3,8,'2020-11-22','14:00:00',18,'P','2020-11-22 15:08:15'),(11,3,8,'2020-11-26','12:00:00',28,'P','2020-11-26 03:56:58'),(12,3,8,'2020-11-26','18:00:00',18,'P','2020-11-26 19:26:34');
+INSERT INTO `agendamento` VALUES (1,3,8,'2020-11-22','14:00:00',18,'P','2020-11-22 15:08:15'),(12,3,8,'2020-11-26','18:00:00',18,'P','2020-11-26 19:26:34'),(13,3,8,'2020-11-27','10:00:00',28,'P','2020-11-27 02:05:00'),(14,3,8,'2020-11-27','12:30:00',10,'P','2020-11-27 02:05:14'),(15,3,8,'2020-11-27','13:30:00',10,'P','2020-11-27 02:12:40');
 /*!40000 ALTER TABLE `agendamento` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -74,7 +74,7 @@ CREATE TABLE `agendamento_servico` (
 
 LOCK TABLES `agendamento_servico` WRITE;
 /*!40000 ALTER TABLE `agendamento_servico` DISABLE KEYS */;
-INSERT INTO `agendamento_servico` VALUES (12,1);
+INSERT INTO `agendamento_servico` VALUES (12,1),(13,1),(13,2),(14,2),(15,2);
 /*!40000 ALTER TABLE `agendamento_servico` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -439,6 +439,38 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `PROC_SERVICO_MAIS_REQUISITADO` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ZERO_IN_DATE,NO_ZERO_DATE,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PROC_SERVICO_MAIS_REQUISITADO`(
+	IN data_atual DATE,
+	IN id_barbearia INT
+)
+BEGIN
+	select 
+		servico.nome,
+		count(servico.nome) as "quantidade"
+	from agendamento_servico
+	inner join agendamento
+	on agendamento_servico.agendamento = agendamento.id_agendamento
+	inner join servico
+	on agendamento_servico.servico = servico.id_servico
+	where data_agendamento = data_atual and agendamento.barbearia = id_barbearia
+	group by servico.nome asc
+    limit 1;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `PROC_UP_USER` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -477,4 +509,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-26 22:49:52
+-- Dump completed on 2020-11-26 23:36:12
