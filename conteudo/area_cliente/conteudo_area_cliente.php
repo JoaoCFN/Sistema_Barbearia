914620f0edcefc3e7a5312024e93956b7e86dfb1
João Neto <joaoneto.20fsa@gmail.com>
2020-11-27 19:58:13 -0300

    Atualização banco de dados

--- conteudo/area_cliente/conteudo_area_cliente.php
+++ conteudo/area_cliente/conteudo_area_cliente.php
@@ -118,7 +118,33 @@
                                 <div class="col-sm-10">
                                     <select class="form-control sb-form-input" name="horario-barbearia">
                                         <option>Selecione o horário desejado</option>
+                                        <option>07:00</option>
+                                        <option>07:30</option>
+                                        <option>08:00</option>
+                                        <option>08:30</option>
                                         <option>09:00</option>
+                                        <option>09:30</option>
+                                        <option>10:00</option>
+                                        <option>10:30</option>
+                                        <option>11:00</option>
+                                        <option>11:30</option>
+                                        <option>12:00</option>
+                                        <option>12:30</option>
+                                        <option>13:00</option>
+                                        <option>13:30</option>
+                                        <option>14:00</option>
+                                        <option>14:30</option>
+                                        <option>15:00</option>
+                                        <option>15:30</option>
+                                        <option>16:00</option>
+                                        <option>16:30</option>
+                                        <option>17:00</option>
+                                        <option>17:30</option>
+                                        <option>18:00</option>
+                                        <option>18:30</option>
+                                        <option>19:00</option>
+                                        <option>19:30</option>
+                                        <option>20:00</option>
                                     </select>
                                 </div>
                             </div>
