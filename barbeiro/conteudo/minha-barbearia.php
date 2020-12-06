c565ba73453f0121e425c7f8d07d538967e036cd
EliabeSilva <eliabe13fsa@hotmail.com>
2020-12-06 18:24:55 -0300

    up

--- barbeiro/conteudo/minha-barbearia.php
+++ barbeiro/conteudo/minha-barbearia.php
@@ -1,3 +1,4 @@
+
 <div class="main mt-5 container-fluid">
     <div class="row">
         <div class="col-12">
@@ -8,7 +9,7 @@
                     </h4>
                 </div>
                 <div class="profile-a">
-                    <form class="example-form">
+                    <form method="POST" action="" class="example-form">
                         <section class="example-section row">
                             <div class="col-md-7 div-sobre">
                                 <a type="button">
@@ -17,8 +18,9 @@
                                     </h5>
                                 </a>
                                 <br>
-                                <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Ex. Tenho uma bela e requisitada barbearia." [required]="isEdit" rows="6"></textarea>
-                                <button type="button" class="btn btn-primary mt-5"> Salvar Alteração</button>
+                                <textarea class="form-control is-invalid" id="validationTextarea" placeholder="Ex. Tenho uma bela e requisitada barbearia." [required]="isEdit" rows="6"> <?php
+                                echo $_SESSION['sobre_barber'];?> </textarea>
+                                <button type="submit" class="btn btn-primary mt-5"> Salvar Alteração</button>
                             </div>
                             <hr>
                             <div class="mt-4 col-md-4 mr-5" style="text-align: center;">
@@ -28,7 +30,7 @@
                                 <div>
                                     <img class="img-thumbnail" width="400" src="assets\images\Cliente-sem-ft.png">
                                 </div>
-                                <button class="mt-3 mb-5 btn btn-primary" type="button">
+                                <button class="mt-3 mb-5 btn btn-primary" type="submit">
                                     Alterar imagem
                                 </button>
                             </div>
@@ -50,51 +52,59 @@
                     <div class="row pt-1 pb-1 mb-2">
                         <div class="col-md-4" style="text-align: center;">
                             <label>Nome estabelecimento</label>
-                            <input type="text" disabled class="form-control" value="Barbearia do seu Zé">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['nome_barbearia']; ?>">
                         </div>
 
                         <div class="col-md-4" style="text-align: center;">
                             <label>Usuário</label>
-                            <input type="text" disabled class="form-control" value="ze_barber">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['nome_dono']; ?>">
                         </div>
 
                         <div class="col-md-4" style="text-align: center;">
                             <label>Email</label>
-                            <input type="text" disabled class="form-control" value="ze_barber@gmail.com">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['email_dono']; ?>">
                         </div>
                     </div>
 
                     <div class="row pt-1 pb-1 mb-2">
                         <div class="col-md-4" style="text-align: center;">
                             <label>Estado</label>
-                            <input type="text" disabled class="form-control" value="BA">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['uf']; ?>">
                         </div>
 
                         <div class="col-md-4" style="text-align: center;">
                             <label>Cidade</label>
-                            <input type="text" disabled class="form-control" value="Feira de Santana">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['cidade']; ?>">
                         </div>
 
                         <div class="col-md-4" style="text-align: center;">
                             <label>CEP</label>
-                            <input class="form-control" maxlength="9" disabled placeholder="Ex. 00000-000" #cep value="44051-682">
+                            <input name="cep" class="form-control" maxlength="9" disabled placeholder="Ex. 00000-000" #cep value="<?php 
+                            echo $_SESSION['cep']; ?>">
                         </div>
                     </div>
 
                     <div class="row pt-1 pb-1 mb-2">
                         <div class="col-md-8" style="text-align: center;">
                             <label>Endereço</label>
-                            <input type="text" disabled class="form-control" value="George Americo, Rua É">
+                            <input type="text" disabled class="form-control" value="<?php 
+                            echo $_SESSION['bairro'].', '.$_SESSION['rua']; ?>">
                         </div>
 
                         <div class="col-md-4" style="text-align: center;">
                             <label>Número</label>
-                            <input type="number" disabled class="form-control" value="1997">
+                            <input id="input" type="number" disabled class="form-control" value="<?php 
+                            echo intVal($_SESSION['num_bar']);?>" />
                         </div>
                     </div>
 
                     <div style="text-align: end;">
-                        <button class="btn btn-primary" style="width: 33%;">
+                        <button id="editar" onclick="editarInfo();" type="button" class="btn btn-primary" style="width: 33%;">
                             Editar
                         </button>
                     </div>
@@ -128,7 +138,8 @@
                                                 <label *ngIf="item.trabalha">Abre:</label>
                                             </div>
                                             <div class="col-md" style="text-align: left;">
-                                                <input *ngIf="item.trabalha" type="time">
+                                                <input *ngIf="item.trabalha" type="time" value="<?php 
+                            echo $_SESSION['horario_abertura']; ?>">
                                             </div>
                                         </div>
                                     </div>
@@ -138,7 +149,8 @@
                                                 <label *ngIf="item.trabalha">Fecha:</label>
                                             </div>
                                             <div class="col-md" style="text-align: left;">
-                                                <input *ngIf="item.trabalha" type="time">
+                                                <input *ngIf="item.trabalha" type="time" value="<?php 
+                            echo $_SESSION['horario_fechamento']; ?>">
                                             </div>
                                         </div>
                                     </div>
@@ -156,3 +168,35 @@
         </div>
     </div>
 </div>
+<script>
+const edit = document.getElementById('editar')
+    edit.type = "button"
+    edit.innerHTML = "Editar"
+function editarInfo(){
+    var x = document.getElementsByTagName("input")
+    
+    if(edit.innerHTML == "Editar"){
+        for (i = 0; i < x.length; i++) {
+        x[i].disabled = false
+        }
+        edit.innerHTML = "Salvar"
+        
+    }
+    else{
+        edit.type = "submit"
+        
+    }
+    
+}
+
+function enviarPost(){
+
+}
+</script>
+<?php
+
+// $conn = mysqli_connect("localhost", "root", "", "dbtcc");
+// $update = "UPDATE barbearia SET sobre_barber='Teste sobre da barber', nome_dono='$_SESSION[nome_dono]', nome_barbearia='$_SESSION[nome_barbearia]', email_dono='$_SESSION[email_dono]', cidade='$_SESSION[cidade]', num_bar='$_SESSION[num_bar]', bairro='$_SESSION[bairro]', cep='$_SESSION[cep]', rua='$_SESSION[rua]' WHERE barbearia_id='$_SESSION[barbearia_id]'";
+// $result = mysqli_query($conn, $update);
+?>
+
--- barbeiro/conteudo/servicos-agendados.php
+++ barbeiro/conteudo/servicos-agendados.php
@@ -1,3 +1,10 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "dbtcc");
+$query = ("SELECT * from agendamento WHERE barbearia='$_SESSION[barbearia_id]'");
+$result = mysqli_query($conn, $query);
+print_r($result);
+    
+?>
 <div class="container-fluid pt-5 pb-5">
     <div class="profile-header">
         <h2>
@@ -6,6 +13,7 @@
     </div>
     <div class="profile-a pt-5" style="display: block; overflow: auto;">
         <table id="tabela_servicos_agendados" class="display">
+        <div>
             <thead>
                 <tr>
                     <th>id</th>
@@ -16,17 +24,33 @@
                     <th>#</th>
                 </tr>
             </thead>
-            <tbody>
-                <tr>
+            </div>
+            <tbody id="agend">
+                
+               
+            </tbody>
+            
+            
+        </table>
+    </div>
+    
+</div>
+<script>
+window.onload = addAgendamentos()
+var agend = document.getElementById('agend')
+function addAgendamentos(){
+    const agend = document.getElementById('agend')
+    console.log(agend.innerHTML)
+
+    agend.innerHTML = `<tr>
                     <td>1</td>
                     <td>Rafael</td>
                     <td>25 R$</td>
                     <td>30 mim</td>
                     <td>Em aberto</td>
                     <td>Ações</td>
-                </tr>
-               
-            </tbody>
-        </table>
-    </div>
-</div>
+                </tr>`
+}
+
+
+</script>
