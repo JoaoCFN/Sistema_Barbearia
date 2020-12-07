e1430b115d271103b0d3518e43bfbe70a285c23e
João Neto <joaoneto.20fsa@gmail.com>
2020-12-07 16:11:22 -0300

    Update meus_charts.js

--- barbeiro/js/charts/meus_charts.js
+++ barbeiro/js/charts/meus_charts.js
@@ -2,16 +2,14 @@ var ctx = document.getElementById('chartServicos').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
-    //responsive: 'true',
     // The data for our dataset
-
     data: {
-        labels: ['08h', '09h', '10h', '11h', '12h', '13h', '14h', '15h', '16h', '17h', '18h', ],
+        labels: ['01/12', '02/12', '03/12', '04/12', '05/12', '06/12', '07/12', '08/12', '09/12', '10/12', '12/12', '12/12', '13/12', '14/12', '15/12'],
         pointHoverRadius: 10,
         lineRadius: 10,
         borderWidth: 10,
         datasets: [{
-            label: 'Quantida de chamados no dia',
+            label: 'Quantida de chamados no dia:',
             labelColor: '#fff',
             backgroundColor: '#52BE48',
             borderColor: '#fff',
@@ -21,15 +19,13 @@ var chart = new Chart(ctx, {
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgba(148,159,177,0.8)',
             borderWidth: 5,
-            data: [65, 59, 80, 81, 56, 55, 40, 33, 22, 45, 85, 65, 75],
+            data: [10, 23, 25, 12, 45, 12, 11, 9, 26, 54, 36, 32, 34, 60, 45],
             borderWidth: 5,
             lineWidth: 5,
             pointRadius: 5,
             label: 'Serviços diarios',
         }]
     },
-
-
     options: {
         responsive: true,
         maintainAspectRatio: false,
@@ -84,7 +80,7 @@ var chart = new Chart(ctx2, {
     type: 'line',
     // The data for our dataset
     data: {
-        labels: ['01/11', '02/11', '03/11', '04/11', '05/11', '06/11', '07/11', '08/11', '09/11', '10/11', '11/11', '12/11', '13/11', '14/11', '15/11', '16/11', ],
+        labels: ['01/11', '02/11', '03/11', '04/11', '05/11', '06/11', '07/11', '08/11', '09/11', '10/11', '11/11', '12/11', '13/11', '14/11', '15/11' ],
         pointHoverRadius: 10,
         lineRadius: 10,
         borderWidth: 10,
@@ -99,7 +95,7 @@ var chart = new Chart(ctx2, {
             pointHoverBackgroundColor: '#fff',
             pointHoverBorderColor: 'rgba(148,159,177,0.8)',
             borderWidth: 5,
-            data: [65, 59, 80, 81, 56, 55, 40, 500, 200, 99, 36, 110, 340, 400, 200, 2000, 560, 450, 99, 100, 125, 75, ],
+            data: [99, 123, 156, 110, 340, 123, 200, 654, 560, 450, 888, 500, 694, 880, 760],
             borderWidth: 5,
             lineWidth: 5,
             pointRadius: 5,
